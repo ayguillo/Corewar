ce8bc74028c6e230fd2e0a2ee48a36ee4e15d29f
Vivien LAMBERT <vlambert@e2r6p15.42.fr>
2019-06-19 12:01:15 +0200

    norm & add proc in vm.h

--- includes/vm.h
+++ includes/vm.h
@@ -6,7 +6,7 @@
 /*   By: vlambert <vlambert@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/17 11:23:18 by vlambert          #+#    #+#             */
-/*   Updated: 2019/06/19 09:31:42 by vlambert         ###   ########.fr       */
+/*   Updated: 2019/06/19 12:00:52 by vlambert         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,16 +14,31 @@
 # define VM_H
 
 # include "../includes/op.h"
+# include "../includes/errors.h"
+
+typedef struct		s_proc
+{
+	struct s_proc	*next;
+	int				number;
+	int				lives;
+	int				last_live;
+	int				alive;
+	int				carry;
+	int				pc;
+	int				waiting;
+}					t_proc;
 
 typedef struct		s_player
 {
 	int				number;
-	int				lives;
-	int				last_lives;
-	int				alive;
-	int				carry;
+	int				period_lives;
+	int				last_p_lives;
+	int				last_live;
+	int				size;
 	char			name[PROG_NAME_LENGTH];
 	char			comment[COMMENT_LENGTH];
+	char			code[CHAMP_MAX_SIZE + 1];
+	t_proc			*proc;
 }					t_player;
 
 typedef struct		s_vm
@@ -34,9 +49,10 @@ typedef struct		s_vm
 	int				cycles;
 	int				cycle_to_die;
 	int				cycles_limit;
+	int				process;
 	int				options;
 	int				players_nbr;
-	char			err[10][500];
+	char			err[10][ERR_MSG_SIZE];
 }					t_vm;
 
 int					options(int ac, char *av[], t_vm *vm);
--- vm/vm_options.c
+++ vm/vm_options.c
@@ -6,99 +6,101 @@
 /*   By: vlambert <vlambert@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/17 11:28:13 by vlambert          #+#    #+#             */
-/*   Updated: 2019/06/18 14:04:51 by vlambert         ###   ########.fr       */
+/*   Updated: 2019/06/19 10:37:24 by vlambert         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/options.h"
 #include "../includes/vm.h"
 #include "../libft/libft.h"
-#include "../includes/errors.h"
 
-static int  player_exists(int player_nbr, t_vm *vm)
+static int	player_exists(int player_nbr, t_vm *vm)
 {
-    int     i;
+	int		i;
 
-    i = 0;
-    while (i < MAX_PLAYERS)
-    {
-        if (vm->players[i].number == player_nbr)
-            return (1);
-        i++;
-    }
-    return (0);
+	i = 0;
+	while (i < MAX_PLAYERS)
+	{
+		if (vm->players[i].number == player_nbr)
+			return (1);
+		i++;
+	}
+	return (0);
 }
 
-static int  opt_dump(int ac, char *av[], t_vm *vm, int i)
+static int	opt_dump(int ac, char *av[], t_vm *vm, int i)
 {
-    int nbr;
+	int		nbr;
 
-    if (vm->cycles_limit)
-        return (ERR_DBLDUMP);
-    if (i < ac && (nbr = ft_atoi(av[i])) > 0)
-        vm->cycles_limit = nbr;
-    else
-        return (ERR_DUMPVAL);
-    return(0);
+	if (vm->cycles_limit)
+		return (ERR_DBLDUMP);
+	if (i < ac && (nbr = ft_atoi(av[i])) > 0)
+		vm->cycles_limit = nbr;
+	else
+		return (ERR_DUMPVAL);
+	return (0);
 }
 
-static int  opt_n(int ac, char *av[], t_vm *vm, int i)
+static int	opt_n(int ac, char *av[], t_vm *vm, int i)
 {
-    int nbr;
+	int		nbr;
 
-    if (vm->players[vm->players_nbr].number)
-        return (ERR_TWONBR);
-    if (i < ac && (nbr = ft_atoi(av[i])) > 0 && !player_exists(nbr, vm))
-        vm->players[vm->players_nbr].number = nbr;
-    else
-        return (ERR_BADNBR);
-    return (0);
+	if (vm->players[vm->players_nbr].number)
+		return (ERR_TWONBR);
+	if (i < ac && (nbr = ft_atoi(av[i])) > 0 && !player_exists(nbr, vm))
+		vm->players[vm->players_nbr].number = nbr;
+	else
+		return (ERR_BADNBR);
+	return (0);
 }
 
-static int  opt_others(int ac, char *av[], t_vm *vm, int i)
+static int	opt_others(int ac, char *av[], t_vm *vm, int i)
 {
-    int j;
+	int		j;
 
-    j = 0;
-    while (av[i][++j])
-    {
-        if (av[i][j] == 'V')
-            vm->options = vm->options | OPTMAJV;
-        else if (av[i][j] == 'v')
-            vm->options = vm->options | OPTV;
-        else if (av[i][j] == 'd')
-            vm->options = vm->options | OPTD;
-        else
-            return (ERR_BADARG);
-    }
-    return (j != 1 ? 0 : ERR_BADARG);
+	j = 0;
+	while (av[i][++j])
+	{
+		if (av[i][j] == 'V')
+			vm->options = vm->options | OPTMAJV;
+		else if (av[i][j] == 'v')
+			vm->options = vm->options | OPTV;
+		else if (av[i][j] == 'd')
+			vm->options = vm->options | OPTD;
+		else
+			return (ERR_BADARG);
+	}
+	return (j != 1 ? 0 : ERR_BADARG);
 }
 
-int         options(int ac, char *av[], t_vm *vm)
+int			options(int ac, char *av[], t_vm *vm)
 {
-    int i;
-    int err;
+	int		i;
+	int		err;
 
-    i = 0;
-    while (++i < ac)
-    {
-        if (av[i][0] == '-')
-        {
-            if (!ft_strcmp(av[i], "-dump"))
-                err = opt_dump(ac, av, vm, ++i);
-            else if (av[i][1] == 'n' && !(av[i][2]))
-                err = opt_n(ac, av, vm, ++i);
-            else
-                err = opt_others(ac, av, vm, i);
-        }
-        else
-        {
-            ft_printf("%s\n", av[i]);
-            vm->players_nbr += 1;
-            /*creer fonction pour ajout du nom et lecture .cor */
-        }
-        if (err)
-            return (err);
-    }
-    return (0);
+	i = 0;
+	err = 0;
+	while (++i < ac)
+	{
+		if (av[i][0] == '-')
+		{
+			if (!ft_strcmp(av[i], "-dump"))
+				err = opt_dump(ac, av, vm, ++i);
+			else if (av[i][1] == 'n' && !(av[i][2]))
+				err = opt_n(ac, av, vm, ++i);
+			else
+				err = opt_others(ac, av, vm, i);
+		}
+		else
+		{
+			ft_printf("%s\n", av[i]);
+			vm->players_nbr += 1;
+			/*
+			**creer fonction pour ajout du nom et lecture .cor
+			*/
+		}
+		if (err)
+			return (err);
+	}
+	return (0);
 }
