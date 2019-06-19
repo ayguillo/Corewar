a9347646d4c496ff4c30583938f9df637ffefaa0
Vivien LAMBERT <vlambert@e2r6p15.42.fr>
2019-06-19 09:34:18 +0200

    norm .h

--- includes/errors.h
+++ includes/errors.h
@@ -6,14 +6,16 @@
 /*   By: vlambert <vlambert@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/17 11:55:22 by vlambert          #+#    #+#             */
-/*   Updated: 2019/06/18 13:42:06 by vlambert         ###   ########.fr       */
+/*   Updated: 2019/06/19 09:34:03 by vlambert         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef ERRORS_H
 # define ERRORS_H
 
-/* Errors codes */
+/*
+**Errors codes
+*/
 # define USAGE          0
 # define ERR_DBLDUMP    1
 # define ERR_DUMPVAL    2
--- includes/vm.h
+++ includes/vm.h
@@ -6,7 +6,7 @@
 /*   By: vlambert <vlambert@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/17 11:23:18 by vlambert          #+#    #+#             */
-/*   Updated: 2019/06/18 13:21:01 by vlambert         ###   ########.fr       */
+/*   Updated: 2019/06/19 09:31:42 by vlambert         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,30 +15,30 @@
 
 # include "../includes/op.h"
 
-typedef struct      s_player
+typedef struct		s_player
 {
-    int     number;
-    int     lives;
-    int     last_lives;
-    int     alive;
-    char    name[PROG_NAME_LENGTH];
-    char    comment[COMMENT_LENGTH];
-}                   t_player;
+	int				number;
+	int				lives;
+	int				last_lives;
+	int				alive;
+	int				carry;
+	char			name[PROG_NAME_LENGTH];
+	char			comment[COMMENT_LENGTH];
+}					t_player;
 
-typedef struct      s_vm
+typedef struct		s_vm
 {
-    unsigned char   mem[MEM_SIZE];
-    t_player        players[MAX_PLAYERS];
-    int             player_turn;
-    int             cycles;
-    int             cycle_to_die;
-    int             cycles_limit;
-    int             options;
-    int             players_nbr;
-    int             carry;
-    char            err[10][500];
-}                   t_vm;
+	unsigned char	mem[MEM_SIZE];
+	t_player		players[MAX_PLAYERS];
+	int				player_turn;
+	int				cycles;
+	int				cycle_to_die;
+	int				cycles_limit;
+	int				options;
+	int				players_nbr;
+	char			err[10][500];
+}					t_vm;
 
-int         options(int ac, char *av[], t_vm *vm);
+int					options(int ac, char *av[], t_vm *vm);
 
 #endif
