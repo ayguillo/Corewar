289bf649904e2fd7fe164b1669174172df3750be
VivienLambert <lambertvivien@yahoo.fr>
2019-06-26 10:23:58 +0200

    Improvement of error handling system

---
+++ vm/vm_errors.c
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   vm_errors.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vlambert <vlambert@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/06/26 10:08:21 by vlambert          #+#    #+#             */
+/*   Updated: 2019/06/26 10:23:46 by vlambert         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/vm.h"
+#include "../libft/libft.h"
+
+/*
+** Check includes/errors.h for message max size ERR_MSG_SIZE
+*/
+void	init_err(t_vm *vm, int *err)
+{
+	*err = 0;
+	ft_strcpy(vm->err[USAGE], "Usage: ./corewar [-d -v -V] \
+[-dump nbr_cycles] [[-n number] champion1.cor] ...\n-d: Debug mode\
+\n-v: Verbose mode\n-V: Visual mode\n");
+	ft_strcpy(vm->err[ERR_DBLDUMP], "More than one dump precised\n");
+	ft_strcpy(vm->err[ERR_BADARG], "Unknown argument\n");
+	ft_strcpy(vm->err[ERR_DUMPVAL], "Dump nbr_cycles is lower than \
+one or missing with option -dump\n");
+	ft_strcpy(vm->err[ERR_BADNBR], "The player number is lower than 1 or \
+missing or the same than another player\n");
+	ft_strcpy(vm->err[ERR_TWONBR], "Two player number were precised \
+for a single player\n");
+	ft_strcpy(vm->err[ERR_OPEN], ": Can't access the champ\n");
+	ft_strcpy(vm->err[ERR_CHAMP], " doesn't seem to be a champion \
+(bad magic number or invalid header)\n");
+	ft_strcpy(vm->err[ERR_MALLOC], " Malloc error\n");
+}
