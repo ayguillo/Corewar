7ef06747b20a29a0f6f03eea17d6f9896cb372ca
Vivien LAMBERT <vlambert@e2r6p15.42.fr>
2019-07-05 15:29:34 +0200

    print.c norm

--- asmsrc/srcs/print.c
+++ asmsrc/srcs/print.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   print.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ayguillo <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: vlambert <vlambert@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/27 17:34:23 by ayguillo          #+#    #+#             */
-/*   Updated: 2019/07/05 13:41:44 by ayguillo         ###   ########.fr       */
+/*   Updated: 2019/07/05 15:29:11 by vlambert         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,22 +30,16 @@ int			printfile(t_header *header, t_file file, t_op **op)
 
 	gnl.line = NULL;
 	gnl.nbline = 0;
-	if (!(ft_retgnl(file.fdopen, &gnl, 1)))
-		return (readerror(&gnl));
-	if ((ft_recupname(header, &gnl, &lenname)) <= 0)
+	if ((ft_retgnl(file.fdopen, &gnl, 1) <= 0 && !(readerror(&gnl)))
+		|| ft_recupname(header, &gnl, &lenname) <= 0
+		|| (ft_retgnl(file.fdopen, &gnl, 1) <= 0 && !(readerror(&gnl)))
+		|| ft_recupcom(header, &gnl, &lencom) <= 0
+		|| !(ft_readinst(file, &gnl, op)))
 		return (0);
-	if (!(ft_retgnl(file.fdopen, &gnl, 1)))
-		return (readerror(&gnl));
-	if (ft_recupcom(header, &gnl, &lencom) <= 0)
-		return (0);
-	if (!(ft_readinst(file, &gnl, op)))
-		return (0);
-	ft_strdel(&(gnl.line));
 	tmp = *op;
-	if (tmp)
-		while (tmp->next)
-			tmp = tmp->next;
-	else
+	while (tmp && tmp->next)
+		tmp = tmp->next;
+	if (!tmp)
 	{
 		ft_dprintf(2, "No instruction\n");
 		return (0);
