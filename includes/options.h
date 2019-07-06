460925d5275448f36397a172b6215ab12aa52c75
VivienLambert <lambertvivien@yahoo.fr>
2019-07-06 15:40:47 +0200

    options.h

--- includes/options.h
+++ includes/options.h
@@ -6,7 +6,7 @@
 /*   By: vlambert <vlambert@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/17 11:06:17 by vlambert          #+#    #+#             */
-/*   Updated: 2019/06/26 15:18:25 by vlambert         ###   ########.fr       */
+/*   Updated: 2019/07/06 15:37:46 by vlambert         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,5 +24,6 @@
 # define OPTV 1
 # define OPTD 1<<1
 # define OPTMAJV 1<<2
+# define OPTZ 1<<3
 
 #endif
