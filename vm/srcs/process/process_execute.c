180313df7063d7fbd8849e896a7582681c649459
bpopov <byurinov1@gmail.com>
2019-07-10 21:57:06 +0200

    [ADD] Debug output and array containing dummy values to store each op corresponding cycles.

--- vm/srcs/process/process_execute.c
+++ vm/srcs/process/process_execute.c
@@ -6,11 +6,11 @@
 /*   By: bopopovi <bopopovi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/25 20:38:51 by bopopovi          #+#    #+#             */
-/*   Updated: 2019/07/02 19:36:36 by bopopovi         ###   ########.fr       */
+/*   Updated: 2019/07/10 19:53:21 by bopopovi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../../../includes/proc.h"
+#include "proc.h"
 
 static bool l_dbg = 1;
 
@@ -29,6 +29,26 @@ void	(*g_op_set[11])(t_vm*, t_proc*) =
 	&op_sti
 };
 
+int		op_cycles[16] =
+{
+	10,
+	10,
+	10,
+	10,
+	10,
+	10,
+	10,
+	10,
+	10,
+	10,
+	10,
+	10,
+	10,
+	10,
+	10,
+	10,
+};
+
 void	exec_op(unsigned char opcode, t_vm *vm, t_proc *process)
 {
 	if (opcode > 0 && opcode <= 11)
@@ -37,9 +57,7 @@ void	exec_op(unsigned char opcode, t_vm *vm, t_proc *process)
 
 int		get_cycles_for_opcode(char opcode)
 {
-	(void)opcode;
-	/* Placeholder value, function to be implemented */
-	return (10);
+	return (op_cycles[opcode - 1]);
 }
 
 int		process_execute(t_vm *vm, t_proc *process)
@@ -49,19 +67,19 @@ int		process_execute(t_vm *vm, t_proc *process)
 
 	pc = process->pc % MEM_SIZE;
 	opcode = vm->mem[pc];
-	ft_putendl(" EXECUTING ");
+	//ft_putendl(" EXECUTING ");
 	local_dbg(l_dbg, "MEMORY STATE :\n");
-	local_dbg(l_dbg, "Instruction opcode : %02x\n", opcode);
-	//display_memory(vm, pc);
+	tmp_print_mem(vm->mem, MEM_SIZE / 4);
 	if (process->waiting > 0)
 		process->waiting -= 1;
 	else if (process->waiting == 0)
 	{
+		local_dbg(l_dbg, "Instruction opcode : %02x\n", opcode);
 		exec_op(opcode, vm, process);
 		process->waiting = -1;
+		local_dbg(l_dbg, "PC state at instruction end : %d\n", process->pc);
 	}
 	else if (process->waiting == -1)
 		process->waiting = get_cycles_for_opcode(opcode);
-	local_dbg(l_dbg, "PC state at instruction end : %d\n", process->pc);
 	return (0);
 }
