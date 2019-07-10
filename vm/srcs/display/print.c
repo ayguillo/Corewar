2183206d864dd622222d458942e20dee9a47c128
Vivien LAMBERT <vlambert@e2r6p15.42.fr>
2019-07-10 11:36:14 +0200

    print.c print_govisu.c norm

--- vm/srcs/display/print.c
+++ vm/srcs/display/print.c
@@ -6,7 +6,7 @@
 /*   By: vlambert <vlambert@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/10 09:33:37 by vlambert          #+#    #+#             */
-/*   Updated: 2019/07/10 11:17:57 by vlambert         ###   ########.fr       */
+/*   Updated: 2019/07/10 11:35:37 by vlambert         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ void	intro_champs(t_vm *vm)
 	int i;
 
 	if (vm->options & OPTMAJV || vm->options & OPTZ)
-		return;
+		return ;
 	i = 0;
 	ft_printf("Introducing contestants...\n");
 	while (i < vm->players_nbr)
@@ -27,7 +27,7 @@ void	intro_champs(t_vm *vm)
 		ft_printf("* Player %d, wheighing %d bytes, \"%s\" (\"%s\") !\n",
 			i + 1, vm->players[i].size, vm->players[i].name,
 			vm->players[i].comment);
-		i ++;
+		++i;
 	}
 	ft_putchar('\n');
 }
@@ -35,7 +35,7 @@ void	intro_champs(t_vm *vm)
 void	print_action(t_vm *vm, t_proc *proc, char *action)
 {
 	if (!(vm->options & OPTV))
-		return;
+		return ;
 	ft_printf("Processus %d from player %d is doing a %s",
 		proc->number, proc->player, action);
 }
@@ -44,7 +44,7 @@ void	print_winner(t_vm *vm)
 {
 	print_arena_govisu(vm, 1);
 	if (vm->options & OPTZ || vm->options & OPTMAJV)
-		return;
+		return ;
 	if (vm->last_player_alive != -1)
 	{
 		ft_printf("AND THE WINNER IS...\n");
