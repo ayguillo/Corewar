9199e9800ec1f4161e67112bab5c6d1fe631cfc8
bpopov <byurinov1@gmail.com>
2019-07-10 21:54:59 +0200

    [ADD] Debug output.

--- vm/srcs/process/read_from_register.c
+++ vm/srcs/process/read_from_register.c
@@ -6,11 +6,13 @@
 /*   By: bopopovi <bopopovi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/27 19:12:00 by bopopovi          #+#    #+#             */
-/*   Updated: 2019/07/01 20:23:54 by bopopovi         ###   ########.fr       */
+/*   Updated: 2019/07/10 20:37:12 by bopopovi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../../../includes/proc.h"
+#include "proc.h"
+
+static bool l_dbg = 1;
 
 int		read_from_register(t_proc *proc, int id)
 {
@@ -18,6 +20,8 @@ int		read_from_register(t_proc *proc, int id)
 
 	if (id > REG_NUMBER)
 		; /* PROC SET STATUS ERROR INVALID REG */
+	local_dbg(l_dbg, "Reading from register %d\n", id);
 	read = proc->regs[id];
+	local_dbg(l_dbg, "Read value 0x%08x from register %d\n\n", read, id);
 	return (read);
 }
