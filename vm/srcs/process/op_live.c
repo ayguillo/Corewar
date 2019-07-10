1fe4994da340263bf9d98e2986015012cf63f562
bpopov <byurinov1@gmail.com>
2019-07-10 21:58:23 +0200

    [FIX] Properly set a player alive by using its index and the new 'get_player_nbr()' function.

--- vm/srcs/process/op_live.c
+++ vm/srcs/process/op_live.c
@@ -6,24 +6,24 @@
 /*   By: bopopovi <bopopovi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/27 19:24:08 by bopopovi          #+#    #+#             */
-/*   Updated: 2019/07/01 20:18:22 by bopopovi         ###   ########.fr       */
+/*   Updated: 2019/07/10 17:35:26 by bopopovi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../../../includes/proc.h"
+#include "proc.h"
 
 static bool l_dbg = 1;
 
 void	op_live(t_vm *vm, t_proc *process)
 {
 	int		live_id;
+	int		player;
 
 	local_dbg(l_dbg, "Instruction op_live()\n");
 	process->pc += T_OPCODE;
 	live_id = read_from_vm(vm, process->pc, T_LDIR);
 	process->pc += T_LDIR;
-	//if (player_exist(vm, live_id))
-	//	set_player_alive(vm, live_id);
-	/* Set player live settings */
+	if ((player = get_player_nbr(vm, live_id)) >= 0)
+		set_player_alive(vm, process, player);
 	local_dbg(l_dbg, "Set player %d as alive\n", live_id);
 }
