aed55cdeb7b8eb4745b3f22a48c2576ded63bce2
bpopov <byurinov1@gmail.com>
2019-07-17 22:54:33 +0200

    [FIX][MOD] Register ID wasn't properly checked and processed, improved debugging output.

--- vm/srcs/process/read_from_register.c
+++ vm/srcs/process/read_from_register.c
@@ -6,7 +6,7 @@
 /*   By: bopopovi <bopopovi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/27 19:12:00 by bopopovi          #+#    #+#             */
-/*   Updated: 2019/07/10 20:37:12 by bopopovi         ###   ########.fr       */
+/*   Updated: 2019/07/17 20:29:53 by bopopovi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,10 +18,13 @@ int		read_from_register(t_proc *proc, int id)
 {
 	int read;
 
-	if (id > REG_NUMBER)
-		; /* PROC SET STATUS ERROR INVALID REG */
-	local_dbg(l_dbg, "Reading from register %d\n", id);
-	read = proc->regs[id];
-	local_dbg(l_dbg, "Read value 0x%08x from register %d\n\n", read, id);
+	read = 0;
+	if (id < 1 || id > REG_NUMBER)
+		local_dbg(l_dbg, "{red}REG_READ INVALID ID : '%d'{eoc}\n", id);
+	else
+	{
+		read = proc->regs[id - 1];
+		local_dbg(l_dbg, "REG_READ value 0x%08x from register %d\n", read, id);
+	}
 	return (read);
 }
