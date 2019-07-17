472b5a879fe2c6fa5e6722ec870c0b5cc0ee2852
bpopov <byurinov1@gmail.com>
2019-07-17 22:56:03 +0200

    [FIX][MOD] Register ID wasn't properly checked and processed, improved debugging output.

--- vm/srcs/process/write_to_register.c
+++ vm/srcs/process/write_to_register.c
@@ -6,14 +6,22 @@
 /*   By: bopopovi <bopopovi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/29 17:27:28 by bopopovi          #+#    #+#             */
-/*   Updated: 2019/07/01 20:25:06 by bopopovi         ###   ########.fr       */
+/*   Updated: 2019/07/17 20:45:48 by bopopovi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../../../includes/proc.h"
+#include "proc.h"
+
+__attribute__((unused))static bool l_dbg = 1;
 
 void	write_to_register(t_proc *process, int register_id, int write)
 {
 	/* ERROR CASE : Invalid register id */
-	process->regs[register_id] = write;
+	if (register_id < 1 || register_id > REG_NUMBER)
+		local_dbg(l_dbg, "REG_WRITE INVALID ID : '%d'\n", register_id);
+	else
+	{
+		local_dbg(l_dbg, "REG_WRITE %d into register %d\n", write, register_id);
+		process->regs[register_id - 1] = write;
+	}
 }
