b935254a4347e056d4a7d7026cbf83e2ee8d13e9
Boris POPOVIC <bopopovi@student.42.fr>
2019-07-22 21:09:59 +0200

    [FIX] Address restriction implies negative values to go 'backward' in the memory. (solved by using signed types for address calculation)

--- vm/srcs/process/calculate_address.c
+++ vm/srcs/process/calculate_address.c
@@ -6,7 +6,7 @@
 /*   By: bopopovi <bopopovi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/20 18:04:25 by bopopovi          #+#    #+#             */
-/*   Updated: 2019/07/22 19:23:48 by bopopovi         ###   ########.fr       */
+/*   Updated: 2019/07/22 21:03:34 by bopopovi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,13 +17,13 @@ __attribute__((unused))static bool l_dbg = 1;
 unsigned int	calculate_address(t_proc *proc, t_op op, unsigned int p1,
 	unsigned int p2)
 {
-	unsigned int addr;
-	unsigned int added;
+	unsigned int	addr;
+	short			added;
 
-	added = proc->pc + (p1 + p2);
+	added = ((short)((short)p1 + (short)p2));
 	if (op.addr_restrict)
 		added = added % IDX_MOD;
-	addr = (added) % MEM_SIZE;
+	addr = proc->pc + added;
 	dbg_print_addr(l_dbg, op, proc, p1, p2);
 	return (addr);
 }
