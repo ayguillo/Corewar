e214341206defe07319486becd4efc2056c1b21f
Boris POPOVIC <bopopovi@student.42.fr>
2019-07-22 19:32:09 +0200

    [FIX] Bad address calculation. (PC wasn't included in the address restriction)

--- vm/srcs/process/calculate_address.c
+++ vm/srcs/process/calculate_address.c
@@ -6,7 +6,7 @@
 /*   By: bopopovi <bopopovi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/20 18:04:25 by bopopovi          #+#    #+#             */
-/*   Updated: 2019/07/20 18:07:52 by bopopovi         ###   ########.fr       */
+/*   Updated: 2019/07/22 19:23:48 by bopopovi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,10 +20,10 @@ unsigned int	calculate_address(t_proc *proc, t_op op, unsigned int p1,
 	unsigned int addr;
 	unsigned int added;
 
-	added = (p1 + p2);
+	added = proc->pc + (p1 + p2);
 	if (op.addr_restrict)
 		added = added % IDX_MOD;
-	addr = (proc->pc + added) % MEM_SIZE;
+	addr = (added) % MEM_SIZE;
 	dbg_print_addr(l_dbg, op, proc, p1, p2);
 	return (addr);
 }
