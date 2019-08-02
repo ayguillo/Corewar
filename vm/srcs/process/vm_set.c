a11b97d482b93f63de953bfe0dae8d2222958e8b
Vivien LAMBERT <vlambert@e2r6p15.42.fr>
2019-08-02 16:01:56 +0200

    set live

--- vm/srcs/process/vm_set.c
+++ vm/srcs/process/vm_set.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   vm_set.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: bopopovi <bopopovi@student.42.fr>          +#+  +:+       +#+        */
+/*   By: vlambert <vlambert@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/27 19:27:05 by bopopovi          #+#    #+#             */
-/*   Updated: 2019/07/17 21:21:56 by bopopovi         ###   ########.fr       */
+/*   Updated: 2019/08/02 16:01:43 by vlambert         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,8 +20,6 @@ void	set_player_alive(t_vm *vm, t_proc *process, int player)
 	local_dbg(l_dbg, "Setting player %d as alive\n", player);
 	vm->players[player].period_lives += 1;
 	vm->last_player_alive = player;
-	process->period_lives += 1;
-	vm->period_lives += 1;
 	if (vm->options & OPTV)
 		print_action(vm, process, "life");
 }
