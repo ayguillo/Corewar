b67d7c511dbedb1475cf75b890b6a322c4edcf4e
Vivien LAMBERT <vlambert@e2r6p15.42.fr>
2019-08-05 13:35:14 +0200

    norme free.c

--- asmsrc/srcs/free.c
+++ asmsrc/srcs/free.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   free.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ayguillo <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: vlambert <vlambert@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/08/02 13:32:57 by ayguillo          #+#    #+#             */
-/*   Updated: 2019/08/02 16:02:30 by ayguillo         ###   ########.fr       */
+/*   Updated: 2019/08/05 13:34:57 by vlambert         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,15 +14,21 @@
 #include "../includes/op.h"
 #include "../includes/asm.h"
 #include "../libft/color.h"
+#include <errno.h>
+#include <stdio.h>
 
+void		ft_free_all(char ***tab, char **str)
+{
+	ft_strdel(str);
+	ft_free_tab2d(tab);
+}
 
 static void	ft_empty(char **str, char ***tab, t_gnl *gnl)
 {
 	int		i;
 
 	i = 0;
-	ft_strdel(str);
-	ft_free_tab2d(tab);
+	ft_free_all(tab, str);
 	ft_dprintf(2, "Empty command at line %i\n%s%s\n", gnl->nbline,
 			_RED_, gnl->line);
 	i = 0;
@@ -31,64 +37,55 @@ static void	ft_empty(char **str, char ***tab, t_gnl *gnl)
 	ft_dprintf(2, "%s%*c%s\n", _GREEN_, ft_strclentab(gnl->line, 0,
 				&gnl->line[i], 0),
 			'^', _RESET_);
-
 }
 
-int			ft_free(char ***tab, int err, t_gnl *gnl, char **str)
+static void	ft_unexp_comment(char ***tab, char **str, t_gnl *gnl)
 {
 	int i;
 	int	tot;
 	int	cal;
 
+	i = -1;
+	tot = 0;
+	while (gnl->line[++i])
+	{
+		if (gnl->line[i] == '\t')
+		{
+			cal = 8 - (tot % 8);
+			tot += cal;
+		}
+		else
+			tot++;
+		if (gnl->line[i] == '.')
+			break ;
+	}
+	ft_dprintf(2, "comment is unexpected at line %i. Error at name.\
+			\n%s%s\n%s%*c%s\n",
+			gnl->nbline, _RED_, gnl->line, _GREEN_, tot, '^', _RESET_);
+	ft_free_all(tab, str);
+}
+
+int			ft_free(char ***tab, int err, t_gnl *gnl, char **str)
+{
 	if (err == 0)
 		ft_empty(str, tab, gnl);
+	else if (err <= 3 && err > 0)
+		ft_free_all(tab, str);
 	if (err == 1)
-	{
-		ft_strdel(str);
-		ft_free_tab2d(tab);
 		ft_dprintf(2, "Champion name too long (Max length %i)\n%sThe name \
-				has %i characters\n%s", PROG_NAME_LENGTH, _RED_, ft_strlen(gnl->line), _RESET_);
-	}
+				has %i characters\n%s", PROG_NAME_LENGTH, _RED_,
+				ft_strlen(gnl->line), _RESET_);
 	if (err == 2)
-	{
-		ft_strdel(str);
-		ft_free_tab2d(tab);
-		ft_putstr_fd("Malloc Error\n", 2);
-	}
+		perror("asm:");
 	if (err == 3)
-	{
-		ft_strdel(str);
-		ft_free_tab2d(tab);
 		ft_dprintf(2, "Comment too long (Max length %i)\n%sYour comment \
-				has %i characters\n%s", COMMENT_LENGTH, _RED_, ft_strlen(gnl->line), _RESET_);
-	}
+				has %i characters\n%s", COMMENT_LENGTH, _RED_,
+				ft_strlen(gnl->line), _RESET_);
 	if (err == 4)
-	{
 		ft_dprintf(2, "Champion too long. End of file at line %i\n",
 				gnl->nbline);
-	}
 	if (err == 5)
-	{
-		i = -1;
-		tot = 0;
-		while (gnl->line[++i])
-		{
-			if (gnl->line[i] == '\t')
-			{
-				cal = 8 - (tot % 8);
-				tot += cal;
-			}
-			else
-				tot++;
-			if (gnl->line[i] == '.')
-				break ;
-		}
-		ft_dprintf(2, "comment is unexpected at line %i. Error at name.\
-				\n%s%s\n%s%*c%s\n",
-				gnl->nbline, _RED_, gnl->line, _GREEN_, tot, '^', _RESET_);
-		ft_strdel(str);
-		ft_free_tab2d(tab);
-	}
+		ft_unexp_comment(tab, str, gnl);
 	ft_strdel(&(gnl->line));
 	return (0);
 }
