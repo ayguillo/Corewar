2b1fa6dc1e395b903a61cf88af82127eec116ffe
Vivien LAMBERT <vlambert@e2r6p15.42.fr>
2019-08-13 11:56:31 +0200

    error on non existing registries

--- vm/srcs/process/get_op_parameters.c
+++ vm/srcs/process/get_op_parameters.c
@@ -6,7 +6,7 @@
 /*   By: vlambert <vlambert@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/27 19:18:39 by bopopovi          #+#    #+#             */
-/*   Updated: 2019/08/12 15:25:46 by vlambert         ###   ########.fr       */
+/*   Updated: 2019/08/13 11:00:23 by vlambert         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -53,7 +53,9 @@ int		get_op_parameters(t_vm *vm, t_proc *proc, t_param *params, t_op op)
 	{
 		if ((parameter_size = get_parameter(vm, proc, &params[i], op)) <= 0)
 			ret = -1;
-			proc->op_pc += parameter_size;
+		if (params[i].type == REG_CODE && (params[i].val > 16 || params[i].val < 1))
+			ret = -1;
+		proc->op_pc += parameter_size;
 		local_dbg(vm->options & OPTD, "\n");
 		i++;
 	}
