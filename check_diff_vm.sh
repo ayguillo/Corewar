b22169fd2b07eda48babadb39483e30008c1f3ce
bpopov <byurinov1@gmail.com>
2019-09-02 12:49:13 +0200

    [UPD] Vm checking script improvements.

--- check_diff_vm.sh
+++ check_diff_vm.sh
@@ -1,33 +1,37 @@
 #!/bin/bash
 
+readonly GREEN="\e[92m"
+readonly RED="\e[31m"
+readonly CLR="\e[0;m"
+
 PLAYER_1="Gagnant.cor"
 PLAYER_2="NoIdea.cor"
 ZAZ_VM="./resources/corewar"
 MY_VM="./corewar"
 ZAZ_ASM="./resources/asm"
-CYCLES_TO_CHECK=2300
-CYCLES_INTERVAL=50
+CYCLE_TO_CHECK=2000
 DIFFS_FILE="vm_diffs.log"
+NBR_OF_CHAMPS=4
 
 function record_diff
 {
-	p1_name="$(basename $PLAYER_1)"
-	p2_name="$(basename $PLAYER_2)"
-	printf "$p1_name against $p2_name\n" > $DIFFS_FILE
+	printf "#################################################################\n" >> $DIFFS_FILE
+	printf "%s %s %s %s\n" $p1_name $p2_name $p3_name $p4_name >> $DIFFS_FILE
 	printf "CYCLE $dump_cycle\n" >> $DIFFS_FILE
 	diff <(echo "$zaz_out") <(echo "$my_out") >> $DIFFS_FILE
 }
 
 function check_diff_for_dump
 {
-	echo "Checking $PLAYER_1 against $PLAYER_2"
-	dump_cycle=$1
+	printf "%s %s %s %s\t\t: " $p1_name $p2_name $p3_name $p4_name | expand -t 40
+	dump_cycle=$CYCLE_TO_CHECK
 	zaz_out="$($ZAZ_VM -d $dump_cycle $PLAYER_1 $PLAYER_2 | awk '$1 ~ /^0x/')"
 	my_out="$($MY_VM -dump $((dump_cycle+1)) $PLAYER_1 $PLAYER_2 | awk '$1 ~ /^0x/')"
 	if [ "$zaz_out" != "" ] && [ "$zaz_out" != "$my_out" ]; then
-		echo "DIFF : $PLAYER_1 against $PLAYER_2 at CYCLE $dump_cycle"
+		printf "${RED}%s\n${CLR}" "DIFFERS"
 		record_diff
-		exit
+	else
+		printf "${GREEN}%s\n${CLR}" "OK"
 	fi;
 }
 
@@ -42,24 +46,51 @@ function compile_champs
 	echo "Champions compiled !"
 }
 
+printf "" > $DIFFS_FILE
+
 compile_champs
+
+PLAYER_1=""
+PLAYER_2=""
+PLAYER_3=""
+PLAYER_4=""
+p1_name=""
+p2_name=""
+p3_name=""
+p4_name=""
 
 for champ_1 in resources/compiled_champs/*.cor; do
 	PLAYER_1="$champ_1"
-	for champ_2 in resources/compiled_champs/*.cor; do
-		PLAYER_2="$champ_2"
-		if [ "$champ_1" != "$champ_2" ]; then
-			check_diff_for_dump 1000
-		fi;
-	done
+	p1_name="$(basename $PLAYER_1)"
+	if [ $NBR_OF_CHAMPS == 1 ]; then
+		check_diff_for_dump
+	else
+		for champ_2 in resources/compiled_champs/*.cor; do
+			PLAYER_2="$champ_2"
+			p2_name="$(basename $PLAYER_2)"
+			if [ $NBR_OF_CHAMPS == 2 ]; then
+				if [ "$champ_2" != "$champ_1" ]; then
+					check_diff_for_dump
+				fi
+			elif [ "$champ_2" != "$champ_1" ]; then
+				for champ_3 in resources/compiled_champs/*.cor; do
+					PLAYER_3="$champ_3"
+					p3_name="$(basename $PLAYER_3)"
+					if [ $NBR_OF_CHAMPS == 3 ]; then
+						if [ "$champ_3" != "$champ_2" ] && [ "$champ_3" != "$champ_1" ]; then
+							check_diff_for_dump
+						fi
+					elif [ "$champ_3" != "$champ_2" ] && [ "$champ_3" != "$champ_1" ]; then
+						for champ_4 in resources/compiled_champs/*.cor; do
+							PLAYER_4="$champ_4"
+							p4_name="$(basename $PLAYER_4)"
+							if [ "$champ_4" != "$champ_3" ] && [ "$champ_4" != "$champ_2" ] && [ "$champ_4" != "$champ_1" ]; then
+								check_diff_for_dump
+							fi
+						done
+					fi
+				done
+			fi
+		done
+	fi
 done
-
-exit
-
-cycles=1
-while [ $cycles -le $CYCLES_TO_CHECK ]; do
-	check_diff_for_dump $cycles
-	printf "$cycles "
-	((cycles+=$CYCLES_INTERVAL))
-done
-echo "$PLAYER_1 against $PLAYER_2 : OK !"
