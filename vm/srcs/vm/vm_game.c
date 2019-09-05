bcea6fbab0098a9304d25460fa24dc20a3842727
Vivien LAMBERT <vlambert@e2r6p15.42.fr>
2019-09-05 17:43:20 +0200

    00

--- vm/srcs/vm/vm_game.c
+++ vm/srcs/vm/vm_game.c
@@ -6,7 +6,7 @@
 /*   By: vlambert <vlambert@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/28 09:08:54 by vlambert          #+#    #+#             */
-/*   Updated: 2019/09/05 17:37:28 by vlambert         ###   ########.fr       */
+/*   Updated: 2019/09/05 17:42:51 by vlambert         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -51,7 +51,7 @@ int			game_cycle(t_vm *vm)
 {
 	intro_champs(vm);
 	display_update(vm);
-	while (((vm->cycles += 1) != vm->cycles_limit || vm->cycles_limit == 0)
+	while (((vm->cycles += 1) > vm->cycles_limit || vm->cycles_limit == 0)
 			&& vm->proc && vm->cycle_to_die > 0)
 	{
 		if (vm->display.status.exit)
