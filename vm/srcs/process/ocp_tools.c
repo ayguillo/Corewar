f5962b2b792119f2d3aa3136a20cf4b0ea101bab
bpopov <byurinov1@gmail.com>
2019-09-07 18:11:42 +0200

    [MOD] Split the parameters type reading into 2 functions depending on wether the processed instruction uses the OCP or not, also added protection against invalid OCP types.

--- vm/srcs/process/ocp_tools.c
+++ vm/srcs/process/ocp_tools.c
@@ -6,7 +6,7 @@
 /*   By: vlambert <vlambert@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/29 17:33:31 by bopopovi          #+#    #+#             */
-/*   Updated: 2019/09/06 20:34:06 by bopopovi         ###   ########.fr       */
+/*   Updated: 2019/09/07 00:51:24 by bopopovi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,44 +17,51 @@ int		get_param_type_from_ocp(char ocp, int param_position)
 	return (ocp >> (8 - param_position * 2) & 0x03);
 }
 
-void	set_params(t_param *param, t_op op, char ocp)
+int		valid_type_for_op(int type, t_op op, int param_nbr)
+{
+	if (type == DIR_CODE)
+		return ((T_DIR & op.arg_types[param_nbr]) == T_DIR);
+	else if (type == IND_CODE)
+		return ((T_IND & op.arg_types[param_nbr]) == T_IND);
+	else if (type == REG_CODE)
+		return ((T_REG & op.arg_types[param_nbr]) == T_REG);
+	return (0);
+}
+
+int		set_params_from_ocp(t_param *params, t_op op, char ocp)
+{
+	int i;
+	int type;
+	int errors;
+
+	i = 0;
+	type = 0;
+	errors = 0;
+	while (i < 4)
+	{
+		type = get_param_type_from_ocp(ocp, (i + 1));
+		errors += (i < op.arg_nbr && !valid_type_for_op(type, op, i));
+		params[i].type = type;
+		i++;
+	}
+	return (errors);
+}
+
+void	set_params_from_op_info(t_param *params, t_op op)
 {
 	int i;
 
 	i = 0;
 	while (i < 4)
 	{
-		if (op.has_ocp)
-			param[i].type = get_param_type_from_ocp(ocp, (i + 1));
-		else
-		{
-			if (i == op.arg_nbr)
-				return ;
-			if (op.arg_types[i] == T_REG)
-				param[i].type = REG_CODE;
-			else if (op.arg_types[i] == T_IND)
-				param[i].type = IND_CODE;
-			else if (op.arg_types[i] == T_DIR)
-				param[i].type = DIR_CODE;
-		}
+		if (i == op.arg_nbr)
+			return ;
+		if (op.arg_types[i] == T_REG)
+			params[i].type = REG_CODE;
+		else if (op.arg_types[i] == T_IND)
+			params[i].type = IND_CODE;
+		else if (op.arg_types[i] == T_DIR)
+			params[i].type = DIR_CODE;
 		i++;
 	}
-}
-
-int		ocp_match_instruction_params(t_op op, char ocp, t_vm *vm)
-{
-	int		param_type;
-	int		i;
-
-	param_type = 0;
-	i = 0;
-	local_dbg(vm->options & OPTD, "\nChecking OCP (%d arguments):\n",
-		op.arg_nbr);
-	while (i < op.arg_nbr)
-	{
-		param_type = get_param_type_from_ocp(ocp, i + 1);
-		i++;
-	}
-	local_dbg(vm->options & OPTD, "\n");
-	return (1);
 }
