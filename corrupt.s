61113e2e848255993e0b975cc2b8773a51cd7c3d
Aymeric GUILLOCHET <ayguillo@e1r3p18.42.fr>
2019-09-10 14:17:54 +0200

    champ + usage

--- corrupt.s
+++ corrupt.s
@@ -2,7 +2,6 @@
 
 .comment "Pour la horde"
 
-zjmp	%0
 live	%1
 live	%1
 live	%1
@@ -44,15 +43,18 @@ firstattack:
 	live	%1
 	live	%1
 	ld	%190055683, r2
-	ld	%118030341, r3
-	ld	%150, r5
-	ld	%50, r6
+	ld	%269025285, r3
+	ld %72617232, r8
+	ld %269025280, r9
+	ld	%500, r5
 	loop:
+		sti r8, %0, %-217
+		sti r9, %0, %-220
 		live: live %1
 		sti	r2, %2, r5
 		sti r3, %0, r5
-		add r5, r6, r5
-		add	r16, r16, r16
+		add r5, r5, r5
+		add	r15, r15, r15
 		zjmp %:loop
 
 secondattack:
@@ -64,14 +66,14 @@ secondattack:
 		fork	%:load
 		ld		%190055683, r5
 		ld		%0, r3
-		ld		%10, r7
-		add	r16, r16, r16
+		ld		%10, r16
+		add	r15, r15, r15
 		live	%1
-		zjmp	%366
+		zjmp	%416
 	load:
-		ld		%117506048, r5
+		ld		%268500992, r5
 		ld		%0, r3
-		ld		%14, r7
-		add	r16, r16, r16
+		ld		%14, r16
+		add	r15, r15, r15
 		live %1
-		zjmp	%332
+		zjmp	%382
